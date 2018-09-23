a58ec479613640ff10fa2b3f5cf14c370cd9848e
meidyika <43518184+meidyika@users.noreply.github.com>
2018-09-23 21:29:30 +0700

    Add files via upload

---
+++ syaratnikah.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication12;
+
+/**
+ *
+ * @author ASUS
+ */
+public class syaratnikah {
+    public static void main (String[] args){
+    boolean cantik = false;
+    
+    int umur = 22;
+            
+    if(umur == 23 && cantik==true) {
+        System.out.println("mau bangettt");
+    }
+    else if (umur == 23 && cantik==false){
+        System.out.println("saya pikir duluuu");
+    }
+    else {
+        System.out.println("saya tidak mau");
+    }
+    }
+}
